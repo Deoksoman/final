1468cf305099dfd56c251e687cafea58626bbb48
Deoksoman <toooglitter@gmail.com>
2024-06-16 22:20:38 +0900

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,11 +1,4 @@
-from utils import read_video, save_video
-from trackers import Tracker
-import cv2
 import numpy as np
-from team_assigner import TeamAssigner
-from player_ball_assigner import PlayerBallAssigner
-from draw_line import Drawline
-from minimap import Minimap
 from ultralytics import YOLO
 import streamlit as st
 import pandas as pd
